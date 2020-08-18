41169f977646061b780393a57c1da90c3d7d0fcd
Andrew Westerman <awesterman1507@gmail.com>
2020-08-17 19:22:42 -0700

    Updated some column properties on Item schema

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,10 +12,10 @@ namespace ItemKeeper.Models
         [Column("id")]
         public int ID { get; set; }
 
-        [Column("name")]
+        [Column("name"), MaxLength(50), Required]
         public string Name { get; set; }
 
-        [Column("cost")]
+        [Column("cost"), Required]
         public int Cost { get; set; }
     }
 }
