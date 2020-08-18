3bf25a4c99e1abe69274544a8a46b46cb0eab770
Andrew Westerman <awesterman1507@gmail.com>
2020-08-17 19:07:34 -0700

    App will init an empty item table with seed data

---
+++ Data/DBInitializer.cs
@@ -0,0 +1,35 @@
+﻿using ItemKeeper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemKeeper.Data
+{
+    public static class DBInitializer
+    {
+        public static void Initialize(ItemContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Items.Any())
+            {
+                return;
+            }
+
+            // Seed with items.txt data
+            var items = new Item[]
+            {
+                new Item{Name="ITEM 1", Cost=100},
+                new Item{Name="ITEM 2", Cost=200},
+                new Item{Name="ITEM 1", Cost=250},
+                new Item{Name="ITEM 3", Cost=300},
+                new Item{Name="ITEM 4", Cost=50},
+                new Item{Name="ITEM 4", Cost=40},
+                new Item{Name="ITEM 2", Cost=200},
+            };
+
+            context.Items.AddRange(items);
+            context.SaveChanges();
+        }
+    }
+}
