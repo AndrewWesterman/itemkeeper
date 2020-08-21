6884e93f8ea0d861208e05f5262f9ebcbd2860c1
Andrew Westerman <awesterman1507@gmail.com>
2020-08-20 19:05:47 -0700

    Added empty item list message on Items

--- ClientApp/src/components/Items.tsx
+++ ClientApp/src/components/Items.tsx
@@ -19,62 +19,68 @@ class Items extends PureComponent<ItemsProps> {
         return (
             <div>
                 <h1 className='text-primary'>All Current Items</h1>
-                <p className='float-left'>
-                    Below is a list off all current items catalogued in the
-                    system
-                </p>
-                <button
-                    className='btn btn-primary float-right m-2'
-                    onClick={() => history.push('/add-item')}
-                >
-                    + Add Item
-                </button>
-                <table className='table table-striped'>
-                    <thead>
-                        <tr>
-                            <th className='d-none d-sm-table-cell'>Id</th>
-                            <th>Item Name</th>
-                            <th>Cost</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {items.map((item: Item) => (
-                            <tr key={`${item.name}:${item.cost}`}>
-                                <td className='d-none d-sm-table-cell'>
-                                    {item.id}
-                                </td>
-                                <td>{item.name}</td>
-                                <td>${item.cost}</td>
-                                <td>
-                                    <div className='float-right'>
-                                        <button
-                                            className='btn btn-info mr-2'
-                                            onClick={() => {
-                                                history.push(
-                                                    `/edit-item/${item.id}`
-                                                );
-                                            }}
-                                        >
-                                            Edit
-                                        </button>{' '}
-                                        <button
-                                            className='btn btn-danger mr-1'
-                                            onClick={() => {
-                                                if (item.id)
-                                                    this.props.deleteItem(
-                                                        item.id
-                                                    );
-                                            }}
-                                        >
-                                            Delete
-                                        </button>
-                                    </div>
-                                </td>
+                <div className=''>
+                    <p className='d-inline-block'>
+                        Below is a list off all current items catalogued in the
+                        system
+                    </p>
+                    <button
+                        className='btn btn-primary flex float-right m-2'
+                        onClick={() => history.push('/add-item')}
+                    >
+                        + Add Item
+                    </button>
+                </div>
+                {items === undefined || items.length === 0 ? (
+                    <h3 className='d-flex'>No items found, add some!</h3>
+                ) : (
+                    <table className='table table-striped'>
+                        <thead>
+                            <tr>
+                                <th className='d-none d-sm-table-cell'>Id</th>
+                                <th>Item Name</th>
+                                <th>Cost</th>
+                                <th></th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {items.map((item: Item) => (
+                                <tr key={`${item.name}:${item.cost}`}>
+                                    <td className='d-none d-sm-table-cell'>
+                                        {item.id}
+                                    </td>
+                                    <td>{item.name}</td>
+                                    <td>${item.cost}</td>
+                                    <td>
+                                        <div className='float-right'>
+                                            <button
+                                                className='btn btn-info mr-2'
+                                                onClick={() => {
+                                                    history.push(
+                                                        `/edit-item/${item.id}`
+                                                    );
+                                                }}
+                                            >
+                                                Edit
+                                            </button>{' '}
+                                            <button
+                                                className='btn btn-danger mr-1'
+                                                onClick={() => {
+                                                    if (item.id)
+                                                        this.props.deleteItem(
+                                                            item.id
+                                                        );
+                                                }}
+                                            >
+                                                Delete
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
         );
     }
